35affd512a8e5a4f2bcf82bbeaa740faf82287d8
Gabriel Silvério <97647384+Bieloco02@users.noreply.github.com>
2024-04-22 14:47:24 -0300

    Add files via upload

---
+++ sketch_may18a/ledsem.ino
@@ -0,0 +1,62 @@
+#define ledVermelho 4
+#define ledAmarelo 3
+#define ledVerde 2
+#define Sensor 5
+#define motor 8
+#define predeston 6
+#define predestoff 7
+int Estado;
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(Sensor, INPUT);
+  pinMode(motor, OUTPUT);
+  pinMode(predeston, OUTPUT);
+  pinMode(predestoff, OUTPUT);
+}
+
+void loop() {
+  Estado = digitalRead(Sensor);
+  Serial.println(Estado);
+  if (!Estado) {
+    comPedestre();
+  }
+  else {
+    semPedestre();
+  }
+}
+void semPedestre() {
+  digitalWrite(ledAmarelo, LOW);
+  digitalWrite(ledVermelho, LOW);
+  digitalWrite(motor, LOW);
+  digitalWrite(ledVerde, HIGH);
+  digitalWrite(predestoff, HIGH);
+  digitalWrite(predeston, LOW);
+}
+void comPedestre() {
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarelo, HIGH);
+  delay(2500);
+  digitalWrite(ledAmarelo, LOW);
+  digitalWrite(predeston, HIGH);
+  digitalWrite(predestoff, LOW);
+  digitalWrite(motor, HIGH);
+  digitalWrite(ledVermelho, HIGH);
+  delay(4000);
+  digitalWrite(motor, LOW);
+  delay(550);
+  digitalWrite(predeston, LOW);
+  
+  for (int i = 1; i < 4; i++) {
+    digitalWrite(predestoff, HIGH);
+    delay(500);
+    digitalWrite(predestoff, LOW);
+    delay(500);
+  };
+  digitalWrite(ledVerde, HIGH);
+  digitalWrite(ledVermelho, LOW);
+  digitalWrite(predestoff, HIGH);
+  delay(7855);
+}
