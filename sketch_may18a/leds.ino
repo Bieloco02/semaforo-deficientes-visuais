5340bc1474a1643f85198d65d4199d30f11f93d6
Gabriel Silvério <97647384+Bieloco02@users.noreply.github.com>
2024-04-29 16:20:54 -0300

    Update leds.ino
    Correções do código ;
    inclusão do vibracall para notificar o momento em que o pedestre com deficiência visual pode atravessar.

--- sketch_may18a/leds.ino
+++ sketch_may18a/leds.ino
@@ -2,14 +2,14 @@
 
 #define PIN        6  // pino de controle da fita de led
 #define NUMPIXELS 300 // número de leds presentes na fita
-#define motor 8
-#define Sensor 5
+#define motor 8 // define porta do vibracall
+#define Sensor 5 // define porta do sensor infravermelho
 
 /*Instancia a fita de led
 ======================================================================= */
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-//#define DELAYVAL 0 // Time (in milliseconds) to pause between pixels
+//#define DELAYVAL 0 // Tempo (em milisegundos) pausa entre pixeis
 int Estado;
 void setup() {
   pixels.begin(); // Inicializa a fita de led
@@ -23,60 +23,79 @@ void loop() {
   Estado = digitalRead(Sensor);
   Serial.println(Estado);
   if (Estado) {
-    amarelo();
-    delay(2500);
     verde();
-    delay(4000);
   }
   else {
+    amarelo();
+    digitalWrite(motor, HIGH);
+    delay(400);
+    digitalWrite(motor, LOW);
+    delay(100);
+    digitalWrite(motor, HIGH);
+    delay(400);
+    digitalWrite(motor, LOW);
+    delay(1900);
     vermelho();
-    delay(4000);
-    
+    delay(10);
+
  }
 }
-/*função para acendimento do sinal vermelho dos carros*/
+//função para acendimento do sinal vermelho dos carros
 void vermelho(){
-   
-   //pixels.clear(); // apaga todos os leds
-  /*apaga os leds amarelos e verdes*/
-  for(int i=44; i<300; i++) { 
+  digitalWrite(motor, HIGH);
+  for(int i=44; i<80; i++) { 
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     pixels.show();
     }//fim do for
-  
   //acende o primeiro circulo 
   for(int i=0; i<15; i++) { 
-    pixels.setPixelColor(i, pixels.Color(150, 0, 0));
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
     pixels.show();   // executa o acendimento dos leds na cor desejada.
     }//fim do for
-  
+
   //acende o segundo circulo
     for(int i=19; i<35; i++) { 
-    pixels.setPixelColor(i, pixels.Color(150, 0, 0));
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
     pixels.show();   // executa o acendimento dos leds na cor desejada.
     }
     alerta();
+    delay(4000);
+    digitalWrite(motor, LOW);
+    delay(2000);
+    
+    /*for(int i=44; i<81; i++) { 
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    pixels.show();
+    }//fim do for*/
 }//fim função vermelho
 
-/*função para acendimento do sinal vermelho dos carros*/
+//função para acendimento do sinal vermelho dos carros/
 void amarelo(){
     pixels.clear(); // apaga todos os leds
     for(int i=44; i<60; i++) { 
-    pixels.setPixelColor(i, pixels.Color(150, 150, 0));
+    pixels.setPixelColor(i, pixels.Color(255,191, 0));
     pixels.show();   // executa o acendimento dos leds na cor desejada.
     }//fim do laço for
 }//fim função amarelo
 
+
 void verde(){
-     pixels.clear(); // apaga todos os leds
+  for(int i=0; i<64; i++) { 
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    pixels.show();
+  }
+  for(int i=80; i<300; i++) { 
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    pixels.show();
+  }
     for(int i=64; i<80; i++) { 
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    pixels.show();   // executa o acendimento dos leds na cor desejada.
-    }//fim do laço for
+    pixels.setPixelColor(i, pixels.Color(0, 250, 0));
+    pixels.show();
+    }
 }//fim função verde
 void alerta(){
   for(int i=80; i<300; i++)   { 
-    pixels.setPixelColor(i, pixels.Color(150, 0, 0));
+    pixels.setPixelColor(i, pixels.Color(250, 0, 0));
     pixels.show(); // alerta.
     } 
     delay(100);
@@ -85,7 +104,7 @@ void alerta(){
     pixels.show(); }
     delay(100);
     for(int i=80; i<300; i++) { 
-    pixels.setPixelColor(i, pixels.Color(150, 0, 0));
+    pixels.setPixelColor(i, pixels.Color(250, 0, 0));
     pixels.show(); }
-    delay(2500);
+    delay(1000);
 }
