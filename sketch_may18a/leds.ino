714cdee92ee4339513db1d9108488c2cda2bf4c9
Gabriel Silvério <97647384+Bieloco02@users.noreply.github.com>
2024-04-30 15:31:20 -0300

    Update leds.ino
    aprimoramento do codigo

--- sketch_may18a/leds.ino
+++ sketch_may18a/leds.ino
@@ -26,15 +26,8 @@ void loop() {
     verde();
   }
   else {
+    delay(1000);
     amarelo();
-    digitalWrite(motor, HIGH);
-    delay(400);
-    digitalWrite(motor, LOW);
-    delay(100);
-    digitalWrite(motor, HIGH);
-    delay(400);
-    digitalWrite(motor, LOW);
-    delay(1900);
     vermelho();
     delay(10);
 
@@ -42,57 +35,54 @@ void loop() {
 }
 //função para acendimento do sinal vermelho dos carros
 void vermelho(){
-  digitalWrite(motor, HIGH);
   for(int i=44; i<80; i++) { 
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     pixels.show();
-    }//fim do for
+    }
   //acende o primeiro circulo 
   for(int i=0; i<15; i++) { 
     pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-    pixels.show();   // executa o acendimento dos leds na cor desejada.
-    }//fim do for
+    pixels.show();   
+    }
 
   //acende o segundo circulo
     for(int i=19; i<35; i++) { 
     pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-    pixels.show();   // executa o acendimento dos leds na cor desejada.
+    pixels.show();
     }
+    delay(1500);
+     digitalWrite(motor, HIGH);//liga o vibracall
     alerta();
     delay(4000);
     digitalWrite(motor, LOW);
-    delay(2000);
-    
-    /*for(int i=44; i<81; i++) { 
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show();
-    }//fim do for*/
+    delay(4000);
 }//fim função vermelho
 
-//função para acendimento do sinal vermelho dos carros/
+//função para acendimento do sinal amarelo dos carros
 void amarelo(){
     pixels.clear(); // apaga todos os leds
     for(int i=44; i<60; i++) { 
     pixels.setPixelColor(i, pixels.Color(255,191, 0));
-    pixels.show();   // executa o acendimento dos leds na cor desejada.
-    }//fim do laço for
-}//fim função amarelo
+    pixels.show();
+    }
+    //vibra pra avisar que esta funcionando
+    digitalWrite(motor, HIGH);
+    delay(340);
+    digitalWrite(motor, LOW);
+    delay(80);
+    digitalWrite(motor, HIGH);
+    delay(340);
+    digitalWrite(motor, LOW);
+    delay(1600);
+}
 
 
 void verde(){
-  for(int i=0; i<64; i++) { 
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show();
-  }
-  for(int i=80; i<300; i++) { 
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show();
-  }
     for(int i=64; i<80; i++) { 
     pixels.setPixelColor(i, pixels.Color(0, 250, 0));
     pixels.show();
     }
-}//fim função verde
+}
 void alerta(){
   for(int i=80; i<300; i++)   { 
     pixels.setPixelColor(i, pixels.Color(250, 0, 0));
@@ -107,4 +97,5 @@ void alerta(){
     pixels.setPixelColor(i, pixels.Color(250, 0, 0));
     pixels.show(); }
     delay(1000);
+    pixels.clear();
 }
