fcab62ad8d92242e145d52328da72b1f56bdbd73
Gabriel Silvério <gdesouzasilverio@gmail.com>
2024-08-18 00:36:27 -0300

    Update TinphMaiav2.ino
    Explicacao de certa parte do codigo por meio de comentarios

--- sketch_may18a/TinphMaiav2/TinphMaiav2.ino
+++ sketch_may18a/TinphMaiav2/TinphMaiav2.ino
@@ -1,160 +1,158 @@
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
-#define PIN        6  // pino de controle da fita de led
-#define NUMPIXELS 300 // número de leds presentes na fita
-#define motor 8 // define porta do vibracall
-#define Sensor 5 // define porta do sensor infravermelho
+#define PIN 6          // pino de controle da fita de led
+#define NUMPIXELS 300  // número de leds presentes na fita
+#define motor 8        // define porta do vibracall
+#define Sensor 5       // define porta do sensor infravermelho
+int pos = 0;           //define pos como valendo 0
+Servo myservo;         // nomeia o servo como myservo
 
-/*Instancia a fita de led
-======================================================================= */
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-Servo myservo;
-int pos = 0;
 
 int Estado;
 void setup() {
-  pixels.begin(); // Inicializa a fita de led
-  pixels.clear(); // apaga todos os leds
-  pinMode(Sensor, INPUT); //inclui o sensor infravermelho
-  pinMode(motor, OUTPUT); // inclui o vibracall
-  myservo.attach(9);
+  pixels.begin();          // Inicializa a fita de led
+  pixels.clear();          // apaga todos os leds
+  pinMode(Sensor, INPUT);  //inclui o sensor infravermelho
+  pinMode(motor, OUTPUT);  // inclui o vibracall
+  myservo.attach(9);       //inclui o servo motor
 }
 
 void loop() {
-  Estado = digitalRead(Sensor);
+  Estado = digitalRead(Sensor);  //nomeia estado como sendo a leitura do sensor infravermelho
   Serial.println(Estado);
-  if (Estado) {
+  if (Estado) {  // se o sensor nao estiver lendo nenhum objeto ele liga as luzes do semaforo verde
     verde();
-  }
-  else {
+  } else {  //se o sensor fazer a leitura de algum objeto 
     delay(1000);
-    amarelo();
+    amarelo(); 
     vermelho();
     delay(10);
-
- }
+  }
 }
-void alerta(){
-  for(int i=35; i<205; i++)   { 
+void alerta() {
+  for (int i = 35; i < 205; i++) {
     pixels.setPixelColor(i, pixels.Color(250, 0, 0));
-    pixels.show(); // alerta.
-    } 
-    delay(100);
-    for(int i=35; i<205; i++) { 
+    pixels.show();  // alerta.
+  }
+  delay(100);
+  for (int i = 35; i < 205; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show(); }
-    delay(100);
-    for(int i=35; i<205; i++) { 
+    pixels.show();
+  }
+  delay(100);
+  for (int i = 35; i < 205; i++) {
     pixels.setPixelColor(i, pixels.Color(250, 0, 0));
-    pixels.show(); }
-    delay(4000);
+    pixels.show();
+  }
+  delay(4000);
   digitalWrite(motor, LOW);
   delay(3567);
-  for(int i=0; i<35; i++)   { 
-    pixels.setPixelColor(i, pixels.Color(0,0, 0));
-    pixels.show(); //pedrest
-    } 
-  delay(657);
-  for(int i=0; i<35; i++)   { 
-    pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-    pixels.show(); //pedrest
-    } 
-  delay(657);
-  for(int i=0; i<35; i++)   { 
+  for (int i = 0; i < 35; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show(); //pedrest
-    } 
-  delay(657);
-  for(int i=0; i<35; i++)   { 
-    pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-    pixels.show(); //pedrest
-    } 
-  delay(657);
-  for(int i=0; i<35; i++)   { 
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show(); //pedrest
+    pixels.show();  //pedrest
   }
   delay(657);
-    for(int i=0; i<35; i++)   { 
+  for (int i = 0; i < 35; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-    pixels.show(); //pedrest
-    } 
-  delay(657);
-  for(int i=0; i<35; i++)   { 
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show(); //pedrest
+    pixels.show();  //pedrest
   }
   delay(657);
-    for(int i=0; i<35; i++)   { 
+  for (int i = 0; i < 35; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    pixels.show();  //pedrest
+  }
+  delay(657);
+  for (int i = 0; i < 35; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-    pixels.show(); //pedrest
-    } 
-  delay(657); 
-    pixels.clear();
-    for(int i=0; i<35; i++)   { 
+    pixels.show();  //pedrest
+  }
+  delay(657);
+  for (int i = 0; i < 35; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    pixels.show();  //pedrest
+  }
+  delay(657);
+  for (int i = 0; i < 35; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-    pixels.show(); //pedrest
-    } 
+    pixels.show();  //pedrest
+  }
+  delay(657);
+  for (int i = 0; i < 35; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    pixels.show();  //pedrest
+  }
+  delay(657);
+  for (int i = 0; i < 35; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 250, 0));
+    pixels.show();  //pedrest
+  }
+  delay(657);
+  pixels.clear();
+  for (int i = 0; i < 35; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 250, 0));
+    pixels.show();  //pedrest
+  }
 }
 //função para acendimento do sinal vermelho dos carros
-void vermelho(){
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+void vermelho() {
+  for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                    // waits 15ms for the servo to reach the position
+    myservo.write(pos);  // tell servo to go to position in variable 'pos'
+    delay(15);           // waits 15ms for the servo to reach the position
   }
-  //acende o vermelho 
-  for(int i=210; i<250; i++) { 
+  //acende o vermelho
+  for (int i = 210; i < 250; i++) {
     pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-    pixels.show();   
-    }
-    delay(3000);
-  for(int i=0; i<35; i++)   { 
+    pixels.show();
+  }
+  delay(3000);
+  for (int i = 0; i < 35; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 250, 0));
-    pixels.show(); //pedrest
-    } 
-     digitalWrite(motor, HIGH);//liga o vibracall
-    alerta();
-    delay(500);
-}//fim função vermelho
+    pixels.show();  //pedrest
+  }
+  digitalWrite(motor, HIGH);  //liga o vibracall
+  alerta();
+  delay(500);
+}  //fim função vermelho
 
 //função para acendimento do sinal amarelo dos carros
-void amarelo(){
-   for(int i=280; i<300; i++) { 
+void amarelo() {
+  for (int i = 280; i < 300; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     pixels.show();
-    }
-    for(int i=255; i<275; i++) { 
-    pixels.setPixelColor(i, pixels.Color(255,191, 0));
+  }
+  for (int i = 255; i < 275; i++) {
+    pixels.setPixelColor(i, pixels.Color(255, 191, 0));
     pixels.show();
-    }
-    //vibra pra avisar que esta funcionando
-    digitalWrite(motor, HIGH);
-    delay(340);
-    digitalWrite(motor, LOW);
-    delay(80);
-    digitalWrite(motor, HIGH);
-    delay(340);
-    digitalWrite(motor, LOW);
-    delay(1600);
-    for(int i=255; i<275; i++) { 
-    pixels.setPixelColor(i, pixels.Color(0,0, 0));
+  }
+  //vibra pra avisar que esta funcionando
+  digitalWrite(motor, HIGH);
+  delay(340);
+  digitalWrite(motor, LOW);
+  delay(80);
+  digitalWrite(motor, HIGH);
+  delay(340);
+  digitalWrite(motor, LOW);
+  delay(1600);
+  for (int i = 255; i < 275; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     pixels.show();
-    }
+  }
 }
 
 
-void verde(){
-    for (pos = 180; pos >= 0; pos -= 1) {
- myservo.write(pos);
-delay(15);
-}
-    for(int i=280; i<300; i++) { 
+void verde() {
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+  for (int i = 280; i < 300; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 250, 0));
     pixels.show();
-    }
-  for(int i=0; i<35; i++)   { 
+  }
+  for (int i = 0; i < 35; i++) {
     pixels.setPixelColor(i, pixels.Color(250, 0, 0));
-    pixels.show(); //pedrest
-}
+    pixels.show();  //pedrest
+  }
 }
